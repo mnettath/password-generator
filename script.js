967a3d92954a03e7a77e02c6ba040542e825bdd5
Maya N <nettathm@gmail.com>
2023-08-26 16:52:04 -0600

    incorporated the random function into the if statements for each condition, fixed bug in variable names that was returning undefined, added finalPassword variables to return a password from generatePassword, made global variable declarations with arrays longer

--- script.js
+++ script.js
@@ -1,50 +1,140 @@
 // VARIABLE DECLARATIONS
 
-var specialChars = ["+", "$", "~"];
+var specialChars = ["+", "$", "~", "!", "#", "%", "&", "~", "@", "?"];
 // using 3 values for now, can add more later
-var numericChars = ["1", "2", "3"];
-var lowerCaseChars = ["a", "b", "c"];
-var upperCaseChars = ["A", "B", "C"];
+var numericChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var lowerCaseChars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCaseChars = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
 var generateBtn = document.querySelector("#generate"); //storing a reference to the button Generate Password
 
-// a prompt for how many chars they want?
-
 // FUNCTIONS
-
-// this function's purpose is getting a random element from an array
-function random() {
-  // using Math.floor and Math.random
-  // --> pick an array
-
-  var randomIndex = Math.floor(Math.random() * specialChars.length);
-  console.log(randomIndex);
-
-  var randomChar = specialChars[randomIndex];
-  console.log(randomChar);
-}
 
 // purpose: figure out what the user wants in their password
 function generatePassword() {
-  var userCharsLength = prompt(
+  var useCharsLength = prompt(
     "How many characters would you like your password to contain?"
   );
+  if (useCharsLength < 8 || useCharsLength > 128) {
+    prompt("Try again! Your password must be between 8-128 characters");
+  }
 
-  var specialChars = confirm("Click ok to confirm special characters");
+  var useSpecialChars = confirm("Click ok to confirm special characters");
 
-  var numericChars = confirm("Click ok to confirm numeric characters");
+  var useNumericChars = confirm("Click ok to confirm numeric characters");
 
-  var lowerCaseChars = confirm("Click ok to confirm lowerCase characters");
+  var useLowerCaseChars = confirm("Click ok to confirm lowerCase characters");
 
-  var upperCaseChars = confirm("Click ok to confirm upperCase characters");
+  var useUpperCaseChars = confirm("Click ok to confirm upperCase characters");
 
-  console.log(userCharsLength);
-  console.log(specialChars);
-  console.log(numericChars);
-  console.log(lowerCaseChars);
-  console.log(upperCaseChars);
+  console.log(useCharsLength);
+  console.log(useSpecialChars);
+  console.log(useNumericChars);
+  console.log(useLowerCaseChars);
+  console.log(useUpperCaseChars);
+
+  var userOptions = {
+    willHaveSpecChars: useSpecialChars,
+    willHaveNumChars: useNumericChars,
+    willHaveLowChars: useLowerCaseChars,
+    willHaveUpperChars: useUpperCaseChars,
+  };
+
+  var result = [];
+
+  if (userOptions.willHaveSpecChars) {
+    var randomIndex = Math.floor(Math.random() * specialChars.length);
+    var randomChar = specialChars[randomIndex];
+    console.log(randomChar);
+    // lets randomly grab a member from the special character's array
+    // push into the results array
+    result.push(randomChar);
+  }
+
+  if (userOptions.willHaveNumChars) {
+    var randomIndex = Math.floor(Math.random() * numericChars.length);
+    var randomChar = numericChars[randomIndex];
+    console.log(randomChar);
+    result.push(randomChar);
+  }
+
+  if (userOptions.willHaveLowChars) {
+    var randomIndex = Math.floor(Math.random() * lowerCaseChars.length);
+    var randomChar = lowerCaseChars[randomIndex];
+    console.log(randomChar);
+    result.push(randomChar);
+  }
+
+  if (userOptions.willHaveUpperChars) {
+    var randomIndex = Math.floor(Math.random() * upperCaseChars.length);
+    var randomChar = upperCaseChars[randomIndex];
+    console.log(randomChar);
+    result.push(randomChar);
+  }
+
+  console.log(result);
+  var finalPassword = result.join(""); // needed to take everything that was in the array and combine it into a string so the textbox wouldnt say undefined anymore
+  return finalPassword; // return exits out of this function and places final Password into the password var
 }
 
+//   var result = [];
+//eventually, this array will store the random generated password. I will use the push method to add to this array and this is what I will return to the user
+
+// purpose: to pull the password from generatePassword function and actually write it in the text area
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password"); //storing a reference to the text area element
--- test.js
+++ test.js
@@ -1,23 +1,9 @@
-var specialChars = ["+", "$", "~"];
-var numericChars = ["1", "2", "3"];
-var lowerCaseChars = ["a", "b", "c"];
-var upperCaseChars = ["A", "B", "C"];
+var text = ["abcdefghijklmnopqrstuvwxyz"];
+var splitArray = text[0].split("");
+// the zero index tells the new array to start from the 0 index of the old array
+console.log(splitArray);
 
-// this function's purpose is getting a random element from an array
-function random(arr) {
-  // using Math.floor and Math.random
-  // --> pick an array
-
-  //arr = ["+", "$", "~"]
-  var randomIndex = Math.floor(Math.random() * arr.length);
-
-  //   console.log(randomIndex);
-
-  var randomChar = arr[randomIndex];
-  console.log(randomChar);
-}
-
-random(specialChars); // tells JS to execute the function
-random(numericChars);
-random(lowerCaseChars);
-random(upperCaseChars);
+var capArray = text[0].toUpperCase();
+var splitCapArray = capArray.split("");
+console.log(capArray);
+console.log(splitCapArray);
